305dd59a981e55eb152c4b85a33322f3b22cca97
tyebean <flower.webster@gmail.com>
2022-04-20 13:56:55 -0400

    add copy gmail to clipboard

---
+++ js/app.js
@@ -0,0 +1,19 @@
+function myFunction() {
+  /* Get the text field */
+  const copyText = document.getElementById("gmail");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
+
+function outFunc() {
+  const tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copy to clipboard";
+}
